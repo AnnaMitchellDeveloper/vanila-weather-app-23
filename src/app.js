1b57f012e34f1228d3e1ae5aac47070f94b40168
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-02-27 19:59:46 -0500

    Added forecast

--- src/app.js
+++ src/app.js
@@ -21,11 +21,12 @@ function formatDate(timestamp) {
   return `${day}${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mond", "Tues"];
   let forecastHTML = `<div class="row">`;
-  
+
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -47,6 +48,15 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+  
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -71,10 +81,12 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].main);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "7e2628686c212c4bf190971af97349ff";
+  let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -114,4 +126,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Milan");
-displayForecast();
+
